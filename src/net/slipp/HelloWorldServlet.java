480a257415d6fe14d797e5be8390a79deb897fca
이석준 <robin00q@naver.com>
2019-12-27 00:12:48 +0900

    first commit

---
+++ src/net/slipp/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package net.slipp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String name = req.getParameter("name");
+		resp.getWriter().print(name + " Hello World!");
+	}
+}
