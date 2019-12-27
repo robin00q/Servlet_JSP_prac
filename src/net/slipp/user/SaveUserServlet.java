3621731a72100680cc9518a08ac0d2a15d7bd55f
이석준 <robin00q@naver.com>
2019-12-27 21:45:51 +0900

    connect with mysql using JDBC

--- src/net/slipp/user/SaveUserServlet.java
+++ src/net/slipp/user/SaveUserServlet.java
@@ -1,14 +1,13 @@
 package net.slipp.user;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.slipp.db.Database;
 
 @WebServlet("/users/save")
 public class SaveUserServlet extends HttpServlet {
@@ -20,7 +19,15 @@ public class SaveUserServlet extends HttpServlet {
 		String email = request.getParameter("email");
 		
 		User user = new User(userId, password, name, email);
-		Database.addUser(user);
+		
+		UserDAO userDAO = new UserDAO();
+		try {
+			userDAO.addUser(user);			
+		} catch (SQLException e){
+			
+		}
+		
+		
 		
 		response.sendRedirect("/");
 	}
---
+++ src/net/slipp/user/UserDAO.java
@@ -0,0 +1,56 @@
+package net.slipp.user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+	public Connection getConnection() {
+		
+		String url = "jdbc:mysql://localhost:3306/slipp_dev?serverTimezone=Asia/Seoul";
+		String id = "root";
+		String pw = "123123";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(url, id, pw);
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+
+	public void addUser(User user) throws SQLException {
+		String sql = "insert into USERS values(?,?,?,?)";
+		PreparedStatement pstmt = getConnection().prepareStatement(sql);
+		pstmt.setString(1, user.getUserId());
+		pstmt.setString(2, user.getPassword());
+		pstmt.setString(3, user.getName());
+		pstmt.setString(4, user.getEmail());
+		
+		pstmt.executeUpdate();
+	}
+	
+	public User findByUserId(String userId) throws SQLException {
+		String sql = "select * from USERS where userId = ?";
+		PreparedStatement pstmt = getConnection().prepareStatement(sql);
+		pstmt.setString(1,  userId);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		if(!rs.next()) {
+			return null;
+		}
+		
+		return new User(
+				rs.getString("userId"),
+				rs.getString("password"),
+				rs.getString("name"),
+				rs.getString("email"));
+		
+	}
+	
+}
